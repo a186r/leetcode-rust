4e3a367afb923012b0d0a02f17b709a89bde918c
a186r <hi.aewalle@gmail.com>
2019-12-20 18:32:00 +0800

    feat(n0002_add_two_numbers): 越写越看不懂了。。。

--- src/n0002_add_two_numbers.rs
+++ src/n0002_add_two_numbers.rs
@@ -16,3 +16,56 @@
  * Explanation: 342 + 465 = 807.
  *
  */
+pub struct Solution{}
+use super::util::linked_list::{to_list, ListNode};
+use std::intrinsics::truncf32;
+
+impl Solution{
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let (mut l1, mut l2) = (l1, l2);
+        let mut dummy_head = Some(Box::new(ListNode::new(0)));
+        let mut tail = &mut dummy_head;
+        let (mut l1_end, mut l2_end, mut overflow) = (false, false, false);
+        loop {
+            let lhs = match l1 {
+                Some(node) => {
+                    l1 = node.next;
+                    node.val
+                },
+                None => {
+                    l1_end = true;
+                    0
+                },
+            };
+            let rhs = match l2 {
+                Some(node) => {
+                    l2 = node.next;
+                    node.val
+                },
+                None => {
+                    l2_end = true;
+                    0
+                }
+            };
+//            如果l1_end, l2_end结束并且咩有溢出，则返回结果
+            if l1_end && l2_end && !overflow{
+                break dummy_head.unwrap().next;
+            }
+
+            let sum = lhs + rhs + if overflow {1} else { 0 };
+
+            let sum = if sum >= 10 {
+                overflow = true;
+                sum - 10
+            }else {
+                overflow = false;
+                sum
+            };
+            tail.as_mut().unwrap().next = Some(Box::new(ListNode::new(sum)));
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+    }
+}
